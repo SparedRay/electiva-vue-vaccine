e34c7693aa8dbad9b077337735687ed1d96699e9
Alfredo Cabello <acabello@medianet.com.ve>
2021-06-15 10:10:41 -0400

    Boilerplate inicial

---
+++ src/components/Bi.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="img-background"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Bi'
+})
+</script>
+
+<style>
+  .img-background {
+    width: 100vw;
+    height: 90vh;
+
+    background-image: url('/img/bi.jpg');
+    background-size: contain;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+</style>
