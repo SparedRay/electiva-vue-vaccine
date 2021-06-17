85b984b05a390f4066c82d7c4efe2520bb07c67b
Alfredo Cabello <acabello@medianet.com.ve>
2021-06-17 16:17:01 -0400

    Parametrizacion email

--- api/_utils/mailer.ts
+++ api/_utils/mailer.ts
@@ -2,7 +2,7 @@ import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
-  port: 2525,
+  port: Number(process.env.MAIL_PORT),
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS
